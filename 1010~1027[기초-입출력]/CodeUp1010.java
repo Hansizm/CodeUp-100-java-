1bd2618ab4300c244e1e9062d6d0dfa54d25039e
Hansizm <sangyooni120@naver.com>
2022-07-13 19:35:42 +0900

    upload

---
+++ 1010~1027[기초-입출력]/CodeUp1010.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class CodeUp1010 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		n = sc.nextInt();
+		System.out.print(n);
+	}
+}
+
+
