5f35126c6231b09048d6a35ba2aa220d5572a009
Hansizm <sangyooni120@naver.com>
2022-07-15 18:30:29 +0900

    update

---
+++ 1031~1037[기초-출력변환]/CodeUp1032.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class CodeUp1032 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.printf("%h", n);
+	}
+}
---
+++ 1031~1037[기초-출력변환]/CodeUp1036.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class CodeUp1036 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		int n = c;
+		System.out.printf("%d", n);
+	}
+}
